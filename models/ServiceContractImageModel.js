ae5158a557784c68a1b76cc5b98a89697357e9ee
Arun Kumar <arun.kumar@mentorox.com>
2024-09-23 15:51:02 +0500

    initial commit

---
+++ models/ServiceContractImageModel.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const MetaImagesModel = sequelize.define('MetaNoteModel', {
+      id: {type: DataTypes.BIGINT, autoIncrement: true, allowNull: false, primaryKey: true},
+      images:{type:DataTypes.STRING,allowNull:false},
+      service_contract_id:{type:DataTypes.BIGINT,allowNull:false},
+      createdBy: { type: DataTypes.STRING, allowNull: true, },
+      updatedBy: { type: DataTypes.STRING, allowNull: true, },
+      deletedBy: { type: DataTypes.STRING, allowNull: true, },
+    },{
+      tableName: 'service_contract_images',
+      paranoid: true
+    });
+      return MetaImagesModel;
+} 
